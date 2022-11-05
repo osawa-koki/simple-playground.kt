30b92c9cc448374f93c49042d5a593b847b9bae3
osawa-koki <github@osawa.dev>
2022-11-06 06:11:38 +0900

    データクラス。

--- src/main/kotlin/$14_dataclass/main.kt
+++ src/main/kotlin/$14_dataclass/main.kt
@@ -51,6 +51,37 @@ fun main() {
   // この方法では当然ですが、コンストラクタが実行されて、
   // インスタンスが新しく生成されるため、
   // 両者を比較してもfalseになります。
+
+  // ==================================================
+
+  // データクラスのプロパティには、
+  // varとvalの両方を使うことができます。
+  // ただし、varを使うと、
+  // データクラスのインスタンスをコピーするときに、
+  // プロパティの値を変更することができるようになります。
+  val dataclass_var1: DataClass2 = DataClass2("dataClass2", 2)
+  val dataclass_var2: DataClass2 = dataclass_var1.copy(number = 3)
+  println("dataclass_var1 == dataclass_var2: ${dataclass_var1 == dataclass_var2}")
+  // -> dataclass_var1 == dataclass_var2: false
+  // これは、dataclass_var1とdataclass_var2のnumberプロパティの値が異なるためです。
+
+  // 一方、valを使うと、
+  // データクラスのインスタンスをコピーするときに、
+  // プロパティの値を変更することができなくなります。
+  val dataclass_val1: DataClass3 = DataClass3("dataClass3", 3)
+  val dataclass_val2: DataClass3 = dataclass_val1.copy(number = 4)
+  println("dataclass_val1 == dataclass_val2: ${dataclass_val1 == dataclass_val2}")
+  // -> dataclass_val1 == dataclass_val2: true
+  // これは、dataclass_val1とdataclass_val2のnumberプロパティの値が同じだからです。
+
+  // ==================================================
+
+  // データクラスには以下の制約があります。
+  // ・データクラスは、openでないfinalクラスでなければならない。
+  // ・データクラスは、abstractでないsealedでなければならない。
+  // ・データクラスは、主コンストラクタに少なくとも1つのパラメータを持たなければならない。
+  // ・データクラスの主コンストラクタは、プライマリコンストラクタである必要がある。
+
 }
 
 // データクラスの定義
@@ -64,3 +95,16 @@ class NormalClass1(
   val name: String,
   val number: Int
 )
+
+// データクラスの定義
+data class DataClass2(
+  val name: String,
+  var number: Int
+)
+
+// データクラスの定義
+data class DataClass3(
+  val name: String,
+  val number: Int
+)
+
