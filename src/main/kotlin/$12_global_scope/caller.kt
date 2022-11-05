432fadbefc0e0584e24f6130eab5861702887dd2
osawa-koki <github@osawa.dev>
2022-11-06 04:22:05 +0900

    グローバルスコープ。

--- src/main/kotlin/$12_global_scope/caller.kt
+++ src/main/kotlin/$12_global_scope/caller.kt
@@ -1,6 +1,12 @@
 package `$12_global_scope`
 
 fun main() {
+  // グローバルスコープ
+  // 「main.kt」ファイルで定義したグローバルスコープに属する変数と関数を呼び出します。
+  // 「main.kt」という名前でありながら、main関数を有しておらず、
+  // 「caller.kt」ファイルがmain関数を持っているという、
+  // 少し変な構成は無視してください。
+
   println(globalVal)
   println(globalFunc())
 }
